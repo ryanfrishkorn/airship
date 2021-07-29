76be2719ad6c2dbf8f77a086e1ede2a71a9e72e2
Ryan Frishkorn <ryan@bluerift.io>
2021-07-28 23:26:16 -0400

    add action subcommands

--- airship.sh
+++ airship.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 port=7777
-ipaddr=`ifconfig | sed -n -E 's/^[[:space:]]+inet (192\.168\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}) .*/\1/p'`
+ipaddr_prefix="192.168."
+ipaddr_prefix_escaped=`echo $ipaddr_prefix | sed 's/\./\\\./g'`
+ipaddr=`ifconfig | sed -n -E 's/^[[:space:]]+inet ('"$ipaddr_prefix_escaped"'[[:digit:]]{1,3}\.[[:digit:]]{1,3}) .*/\1/p'`
 
 # handle arguments
-if [ -z $1 ] || [ ! -r $1 ]; then
+if [ -z $1 ] || [ -z $2 ]; then
 	script=`basename $0`
-	echo "usage: $script <file>"
+	echo "usage: $script send <file>"
+	echo "       $script get  <ip_addr>"
 	exit 1
 fi
 
-send_file=$1
-send_file_basename=`basename $1`
+action=$1
+send_file=$2
+send_file_basename=`basename $2`
+
+# check actions
+if [ "$action" != "send" ] && [ "$action" != "get" ]; then
+	echo "acceptable actions are: send, get"
+	exit 1
+fi
+
+if [ ! -r $send_file ]; then
+	echo "error reading file $send_file"
+	exit 1
+fi
 
 cmd_tx="nc -l $port < \"$send_file\""
 cmd_rx="nc $ipaddr $port > \"$send_file_basename\""
